a09f80fcb14491f4fe63717a2361e9edc17635ed
DodoGTA GT <tcg96nougat@gmail.com>
2018-02-17 18:49:53 +0200

    Fix the remaining bugs

--- tg_bot/modules/misc2.py
+++ tg_bot/modules/misc2.py
@@ -21,16 +21,19 @@ def birthday(bot: Bot, update: Update, args: List[str]):
     user_id = extract_user(update.effective_message, args)
     if user_id:
         target_user = bot.get_chat(user_id)
-        if target_user.username:
+        if target_user.id == bot.id:
+            msg.reply_text("Bots don't have birthdays, right?")
+            return ""
+        elif target_user.username:
             target_user = "@" + escape_markdown(target_user.username)
         else:
             target_user = "[{}](tg://user?id={})".format(target_user.first_name,
                                                    target_user.id)
-
-    # if no target found, bot bails out
     else:
-      msg.reply_text("I see what you did there...")
-      return ""
+        if not args:
+            msg.reply_text("I see what you did there...")
+        return ""
+
 
     count = random.randint(1, 10)
     repl = format("Happy birthday, {}!\n".format(target_user) * count)
@@ -54,7 +57,7 @@ def nou(bot: Bot, update: Update):
 
 __help__ = """
  - /birthday: wish an user a happy birthday 1-10 times 😂.
- - /nou: (No * 1-50)U
+ - /nou: (No \* [1-50])U
 """
 
 __mod_name__ = "Misc 2"
